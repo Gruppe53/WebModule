5d9a7911d8ed34e9ee13d7f9d293d172217a1ab7
anders <Anders2108@hotmail.com>
2014-06-12 17:45:20 +0200

    Tilføjede test (Main i default)

---
+++ ScaleWebApp/src/Main.java
@@ -0,0 +1,39 @@
+import java.sql.*;
+
+import databaseAccess.*;
+
+public class Main {
+	private static DBAccess db;
+	private static ResultSet test;
+	
+	public static void main(String[] args) throws SQLException, Exception {
+		db = new DBAccess("72.13.93.206", 3307, "gruppe55", "gruppe55", "55gruppe");
+		
+		System.out.println("--- INSERT ---");
+		try {
+			int knald = db.doSqlUpdate("INSERT INTO user VALUES(4, 'Malte Magnussen', '1234567893', '2341', 1)");
+			
+			if(knald > 0)
+				System.out.println("Succes");
+		}
+		catch(SQLException e) {
+			System.out.println("Failure");
+		}
+		
+		
+		System.out.println("--- SELECT ---");
+		test = db.doSqlQuery("SELECT * FROM user");
+		
+		try {
+			while(test.next())
+				System.out.println("u_name: " + test.getString("u_name") + ", u_level: " + test.getInt("u_level"));
+		}
+		catch (SQLException e) {
+			e.printStackTrace();
+		}
+		finally {
+			test.close();
+			db.closeSql();
+		}
+	}
+}
