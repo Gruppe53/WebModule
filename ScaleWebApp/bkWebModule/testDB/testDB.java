f6b08c37d494f346254119b1d1da56c25fe4a3b8
Kristin Hansen <maagenator@gmail.com>
2014-06-12 14:57:26 +0200

    asd

--- ScaleWebApp/bkWebModule/testDB/testDB.java
+++ ScaleWebApp/bkWebModule/testDB/testDB.java
@@ -7,18 +7,33 @@ import daointerfaces.DALException;
 import DBaccess.Connector;
 
 public class testDB {
-	public static void main(String [] args){
-	try { new Connector(); }
-	catch (InstantiationException e) { e.printStackTrace(); }
-	catch (IllegalAccessException e) { e.printStackTrace(); }
-	catch (ClassNotFoundException e) { e.printStackTrace(); }
-	catch (SQLException e) { e.printStackTrace(); }
-	
-	
-	System.out.println("User with user_Id = 1: ");
-	MySQLUserDAO noget = new MySQLUserDAO();
-	try{ System.out.println(noget.getUser(1)); }
-	catch (DALException e) { System.out.println(e.getMessage()); }
-	
-}
+	public static void main(String [] args) {
+		try {
+			new Connector();
+		}
+		catch (InstantiationException e) {
+			e.printStackTrace();
+		}
+		catch (IllegalAccessException e) { 
+			e.printStackTrace();
+		}
+		catch (ClassNotFoundException e) { 
+			e.printStackTrace();
+		}
+		catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		
+		System.out.println("User with user_Id = 1: ");
+		
+		MySQLUserDAO noget = new MySQLUserDAO();
+		
+		try{
+			System.out.println(noget.getUser(1));
+		}
+		catch (DALException e) {
+			System.out.println(e.getMessage());
+		}
+	}
 }
